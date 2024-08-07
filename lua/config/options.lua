cfc60b68bd6eea31b7027928ae1b4e2ae3ddecde
Kyle McCahill <kylemccahill@gmail.com>
2024-08-07 15:55:11 -0500

    updated

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,6 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
--- Check if 'pwsh' is executable and set the shell accordingly
+
 -- Turning off auto format
-vim.g.autoformat = false
+-- vim.g.autoformat = false
---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,19 @@
+return {
+  { "rebelot/kanagawa.nvim", lazy = false, priority = 1000, opts = {} },
+  { "ellisonleao/gruvbox.nvim" },
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      style = "night",
+    },
+  },
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "kanagawa",
+    },
+  },
+}
