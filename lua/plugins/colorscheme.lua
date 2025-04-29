4840def360ae52862f4c643f180021cdf3b97a54
Kyle McCahill <kylemccahill@gmail.com>
2025-04-29 17:06:01 -0500

    fixed light background stuff

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -22,6 +22,8 @@ return {
         -- Optionally configure and load the colorscheme
         -- directly inside the plugin declaration.
         vim.g.everforest_enable_italic = true
+        -- vim.o.background = "light"
+        vim.g.everforest_background = "soft"
         vim.cmd.colorscheme('everforest')
       end
   },
