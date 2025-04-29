12fe6aa56935c68daac4829bd42ff0051c53f664
Kyle McCahill <kylemccahill@gmail.com>
2025-04-29 16:54:27 -0500

    added everforest

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -9,14 +9,26 @@ return {
       style = "storm",
     },
   },
-  { "navarasu/onedark.nvim", lazy = false, priority = 1000, opts = { style = "warmer" } },
   -- { "shaunsingh/nord.nvim" , lazy = false, priority = 1000, opts = {}},
   { "navarasu/onedark.nvim", lazy = false, priority = 1000, opts = { style = "dark" }},
-  -- Configure LazyVim to load gruvbox
+  {
+      'sainnhe/everforest',
+      lazy = false,
+      priority = 1000,
+      opts = {
+            style = 'light'
+      },
+      config = function()
+        -- Optionally configure and load the colorscheme
+        -- directly inside the plugin declaration.
+        vim.g.everforest_enable_italic = true
+        vim.cmd.colorscheme('everforest')
+      end
+  },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "onedark",
+      colorscheme = "everforest",
     },
   },
 }
